b2f38d0320cd2a592166c49babd5cfb847bba731
Soni Bro <shantanusoni979@gmail.com>
2024-12-28 10:08:37 +0530

    Adding component Movie Card.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../styles.css';
+import React from "react";
+import "../styles.css";
 
-export default function Header() {  
-    return (
-        <div className='header'>
-           <img className='logo' src='logo.png' alt='moviedux'/>
-           <h2 className='app-subtitle'> It's time for popcorn and find your next movie here.</h2>
-        </div>
-    );
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="logo" src="logo.png" alt="moviedux" />
+      <h2 className="app-subtitle">
+        {" "}
+        It's time for popcorn and find your next movie here.
+      </h2>
+    </div>
+  );
 }
